86f6970def4052d581f38236bc6ae11ea2064559
kelveenfabian <kelveenfabian@pursuit.org>
2019-04-27 16:56:01 -0400

    Create ResultFragment class

--- app/src/main/java/com/example/statsdontlie/view/MainActivity.java
+++ app/src/main/java/com/example/statsdontlie/view/MainActivity.java
@@ -12,6 +12,6 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        new BDLRepository().makeResponse();
+        new BDLRepository().initReftofitCall(237);
     }
 }
--- app/src/main/java/com/example/statsdontlie/view/fragments/ResultFragment.java
+++ app/src/main/java/com/example/statsdontlie/view/fragments/ResultFragment.java
@@ -2,24 +2,89 @@ package com.example.statsdontlie.view.fragments;
 
 
 import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.TextView;
 
 import com.example.statsdontlie.R;
 
 public class ResultFragment
         extends Fragment {
 
+    public static final String CORRECT = "CORRECT";
+    public static final String WRONG = "WRONG";
+    private int correct;
+    private int wrong;
+    private TextView result;
+    private Button menu;
+    private Button restart;
 
-    public ResultFragment() {
+    public ResultFragment() {}
+
+    public static ResultFragment newInstance(final int correctAnswers, final int wrongAnswers) {
+        ResultFragment resultFragment = new ResultFragment();
+        Bundle bundle = new Bundle();
+
+        bundle.putInt(CORRECT, correctAnswers);
+        bundle.putInt(WRONG, wrongAnswers);
+        resultFragment.setArguments(bundle);
+
+        return resultFragment;
     }
 
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        if (getArguments() != null) {
+            correct = getArguments().getInt(CORRECT);
+            wrong = getArguments().getInt(WRONG);
+        }
+    }
+
+    public void initializeViews(View view){
+        result = view.findViewById(R.id.answer_results);
+        menu = view.findViewById(R.id.menu_btn);
+        restart = view.findViewById(R.id.restart_btn);
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater,
+                             ViewGroup container,
                              Bundle savedInstanceState) {
         return inflater.inflate(R.layout.fragment_result, container, false);
     }
 
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        initializeViews(view);
+
+        final String concat_results = "Correct Answers: " + correct + "\n" + "Wrong Answers: " + wrong;
+        result.setText(concat_results);
+
+        clickEvents();
+    }
+
+    public void clickEvents(){
+        menu.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+
+        restart.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+    }
 }
