1500a609dd30b3a246c1a8a2310b7ad60dda45d9
Eric Diaz <ericdiaz@pursuit.org>
2019-04-27 14:01:59 -0400

    setup network

--- app/src/main/java/com/example/statsdontlie/network/RetrofitSingleton.java
+++ app/src/main/java/com/example/statsdontlie/network/RetrofitSingleton.java
@@ -1,4 +1,34 @@
 package com.example.statsdontlie.network;
 
+import com.example.statsdontlie.constants.BDLAppConstants;
+
+import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
+
 public class RetrofitSingleton {
+
+    private static Retrofit singleInstance;
+    private static BDLService singleService;
+
+    private RetrofitSingleton() {
+    }
+
+    private static Retrofit getSingleInstance() {
+        if (singleInstance == null) {
+            singleInstance = new Retrofit.Builder()
+                    .baseUrl(BDLAppConstants.BASE_URL)
+                    .addConverterFactory(GsonConverterFactory.create())
+                    .build();
+        }
+        return singleInstance;
+    }
+
+    public static BDLService getSingleService() {
+        if (singleService == null) {
+            singleService = getSingleInstance().create(BDLService.class);
+        }
+        return singleService;
+    }
+
+
 }
