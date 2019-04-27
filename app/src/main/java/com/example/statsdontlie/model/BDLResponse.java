af298745a2b42e39edcc4ce272f91837ebffe3b4
kelveenfabian <kelveenfabian@pursuit.org>
2019-04-27 14:49:13 -0400

    Create response class for API parsing

--- app/src/main/java/com/example/statsdontlie/model/BDLResponse.java
+++ app/src/main/java/com/example/statsdontlie/model/BDLResponse.java
@@ -1,4 +1,42 @@
 package com.example.statsdontlie.model;
 
+import com.google.gson.annotations.SerializedName;
+
+import java.util.List;
+
 public class BDLResponse {
+    List<GameStats> data;
+
+    public List<GameStats> getData() {
+        return data;
+    }
+
+    public class GameStats{
+        private int pts;
+        private Player player;
+
+        public int getPts() {
+            return pts;
+        }
+
+        public Player getPlayer() {
+            return player;
+        }
+
+        public class Player{
+            @SerializedName("first_name")
+            private String firstName;
+            @SerializedName("last_name")
+            private String lastName;
+
+
+            public String getFirstName() {
+                return firstName;
+            }
+
+            public String getLastName() {
+                return lastName;
+            }
+        }
+    }
 }
--- app/src/main/java/com/example/statsdontlie/network/BDLService.java
+++ app/src/main/java/com/example/statsdontlie/network/BDLService.java
@@ -1,14 +1,5 @@
 package com.example.statsdontlie.network;
 
-import com.example.statsdontlie.model.BDLResponse;
-
-import retrofit2.Call;
-import retrofit2.http.GET;
-import retrofit2.http.Query;
-
 public interface BDLService {
 
-    @GET()
-    Call<BDLResponse> getResponse(@Query("player_ids[]") Integer player_ids,
-                                  @Query("seasons[]") Integer seasons);
 }
