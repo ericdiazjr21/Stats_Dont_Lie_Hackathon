e78252be7d37f6365273231438d969db64415869
Eric Diaz <ericdiaz@pursuit.org>
2019-04-27 14:50:29 -0400

    fixed network

--- app/src/main/java/com/example/statsdontlie/constants/BDLAppConstants.java
+++ app/src/main/java/com/example/statsdontlie/constants/BDLAppConstants.java
@@ -2,6 +2,7 @@ package com.example.statsdontlie.constants;
 
 public class BDLAppConstants {
     public static final String BASE_URL = "https://www.balldontlie.io";
-
+    public static final String ENDPOINT = "/api/v1/stats";
+    public static final String BDLREPOSITORY_TAG = "BDLRepository";
 
 }
