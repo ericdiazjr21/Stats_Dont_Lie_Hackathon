3a8553af54b8ec634bc90ad220b9cde6272b8df6
kelveenfabian <kelveenfabian@pursuit.org>
2019-04-28 11:16:30 -0400

    fetch more data from api

--- app/src/main/java/com/example/statsdontlie/model/BDLResponse.java
+++ app/src/main/java/com/example/statsdontlie/model/BDLResponse.java
@@ -13,7 +13,42 @@ public class BDLResponse {
 
     public class GameStats{
         private int pts;
+        private int ast;
+        private int blk;
+        private int fg3_pct;
+        private int dreb;
+        private int fg_pct;
+        private int ft_pct;
+        private String min;
         private Player player;
+
+        public int getAst() {
+            return ast;
+        }
+
+        public int getBlk() {
+            return blk;
+        }
+
+        public int getFg3_pct() {
+            return fg3_pct;
+        }
+
+        public int getDreb() {
+            return dreb;
+        }
+
+        public int getFg_pct() {
+            return fg_pct;
+        }
+
+        public int getFt_pct() {
+            return ft_pct;
+        }
+
+        public String getMin() {
+            return min;
+        }
 
         public int getPts() {
             return pts;
