a376674cf77c9ea9f031695e487b045ce79e38ad
Eric Diaz <ericdiaz@pursuit.org>
2019-09-11 20:02:22 -0400

    add more test for edge cases and optimized code

--- app/src/main/java/com/example/statsdontlie/utils/PlayerUtil.java
+++ app/src/main/java/com/example/statsdontlie/utils/PlayerUtil.java
@@ -2,24 +2,48 @@ package com.example.statsdontlie.utils;
 
 import android.support.annotation.NonNull;
 
-public class PlayerUtil {
+/**
+ * Last Updated - 9/11/19
+ * Last Change By - Eric Diaz
+ * <p>
+ * A Utility class for getting player photos
+ */
 
-    private static final String EMPTY_STRING = "";
-    private static final String PHOTO_URL = "https://nba-players.herokuapp.com/players/";
+public final class PlayerUtil {
 
-    public static String createPlayerPhoto(@NonNull final String firstName,
+    private static final String PLAYER_PHOTO_URL = "https://nba-players.herokuapp.com/players/";
+    private static final String DEFAULT_PLAYER_PHOTO_URL = "https://kytopen.com/wp-content/uploads/2018/01/placeholder_avatar-400x400.png";
+
+    private PlayerUtil() {
+    }
+
+    public static String getPlayerPhotoUrl(@NonNull final String firstName,
                                            @NonNull final String lastName) {
 
-        if (!firstName.equals(EMPTY_STRING) && !lastName.equals(EMPTY_STRING)) {
+        return isValidFullName(firstName, lastName) ?
 
-            if (firstName.equals("D'Angelo")) {
+          createPlayerPhotoUrl(firstName, lastName) : DEFAULT_PLAYER_PHOTO_URL;
+    }
 
-                return PHOTO_URL + lastName + "/" + "dangelo";
-
-            }
-            return PHOTO_URL + lastName + "/" + firstName;
+    /**
+     * I used a switch here in case we run into another issue in the future
+     * if decide to add more players.
+     */
+    private static String createPlayerPhotoUrl(@NonNull final String firstName,
+                                               @NonNull final String lastName) {
+        switch (firstName) {
+            case "D'Angelo":
+                return PLAYER_PHOTO_URL + lastName + "/" + "dangelo";
+            default:
+                return PLAYER_PHOTO_URL + lastName + "/" + firstName;
         }
+    }
 
-        return EMPTY_STRING;
+    private static boolean isValidFullName(@NonNull final String firstName,
+                                           @NonNull final String lastName) {
+
+        return firstName != null && lastName != null
+
+          && !firstName.equals("") && !lastName.equals("");
     }
 }
--- app/src/test/java/com/example/statsdontlie/PlayerUtilsTest.java
+++ app/src/test/java/com/example/statsdontlie/PlayerUtilsTest.java
@@ -7,18 +7,31 @@ import org.junit.Test;
 
 public class PlayerUtilsTest {
 
-
     @Test
-    public void GivenEmptyStringWhenCreatePlayerPhotoCalledThenReturnEmptyString() {
+    public void GivenEmptyStringWhenCreatePlayerPhotoCalledThenReturnDefaultPhotoUrl() {
         //Given
         String emptyStringFirstName = "";
         String emptyStringLastName = "";
-        String expectedResult = "";
+        String expectedDefaultPhotoUrl = "https://kytopen.com/wp-content/uploads/2018/01/placeholder_avatar-400x400.png";
 
         //When
-        String playerPhoto = PlayerUtil.createPlayerPhoto(emptyStringFirstName, emptyStringLastName);
+        String playerPhotoUrl = PlayerUtil.getPlayerPhotoUrl(emptyStringFirstName, emptyStringLastName);
 
         //Then
-        Assert.assertEquals(expectedResult, playerPhoto);
+        Assert.assertEquals(expectedDefaultPhotoUrl, playerPhotoUrl);
+    }
+
+    @Test
+    public void GivenNullStringWhenCreatePlayerPhotoCalledThenReturnDefaultPhotoUrl() {
+        //Given
+        String emptyStringFirstName = null;
+        String emptyStringLastName = null;
+        String expectedDefaultPhotoUrl = "https://kytopen.com/wp-content/uploads/2018/01/placeholder_avatar-400x400.png";
+
+        //When
+        String playerPhotoUrl = PlayerUtil.getPlayerPhotoUrl(emptyStringFirstName, emptyStringLastName);
+
+        //Then
+        Assert.assertEquals(expectedDefaultPhotoUrl, playerPhotoUrl);
     }
 }
