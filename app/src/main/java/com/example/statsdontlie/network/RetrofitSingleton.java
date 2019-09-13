c30e17f23837291bbbba5e9216fb618baea9e71a
kelveenfabian <kelveenfabian@pursuit.org>
2019-09-12 20:47:58 -0400

    Modify RetrofitSingleton instance to retry connection when lost

--- app/src/main/java/com/example/statsdontlie/network/RetrofitSingleton.java
+++ app/src/main/java/com/example/statsdontlie/network/RetrofitSingleton.java
@@ -6,6 +6,7 @@ import java.util.concurrent.TimeUnit;
 
 import io.reactivex.schedulers.Schedulers;
 import okhttp3.OkHttpClient;
+import okhttp3.logging.HttpLoggingInterceptor;
 import retrofit2.Retrofit;
 import retrofit2.adapter.rxjava2.RxJava2CallAdapterFactory;
 import retrofit2.converter.gson.GsonConverterFactory;
@@ -20,15 +21,20 @@ public class RetrofitSingleton {
 
     private static Retrofit getSingleInstance() {
         if (singleInstance == null) {
+            HttpLoggingInterceptor interceptor = new HttpLoggingInterceptor();
+            interceptor.setLevel(HttpLoggingInterceptor.Level.BODY);
+
             OkHttpClient client = new OkHttpClient.Builder()
-              .connectTimeout(100, TimeUnit.SECONDS)
-              .readTimeout(100, TimeUnit.SECONDS).build();
+                    .connectTimeout(100, TimeUnit.SECONDS)
+                    .readTimeout(100, TimeUnit.SECONDS)
+                    .retryOnConnectionFailure(true)
+                    .addInterceptor(interceptor).build();
 
             singleInstance = new Retrofit.Builder()
-              .baseUrl(BDLAppConstants.BASE_URL).client(client)
-              .addConverterFactory(GsonConverterFactory.create())
-              .addCallAdapterFactory(RxJava2CallAdapterFactory.createWithScheduler(Schedulers.io()))
-              .build();
+                    .baseUrl(BDLAppConstants.BASE_URL).client(client)
+                    .addConverterFactory(GsonConverterFactory.create())
+                    .addCallAdapterFactory(RxJava2CallAdapterFactory.createWithScheduler(Schedulers.io()))
+                    .build();
         }
         return singleInstance;
     }
